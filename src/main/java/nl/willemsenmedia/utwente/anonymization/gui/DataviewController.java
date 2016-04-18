2e6e1936555e479cc7668ed72fd9441590a0adc1
Martijn Willemsen <m.j.willemsen@student.utwente.nl>
2016-04-18 13:25:24 +0200

    Tabel verbeterd, je kunt ook ctrl - c doen

--- src/main/java/nl/willemsenmedia/utwente/anonymization/gui/DataviewController.java
+++ src/main/java/nl/willemsenmedia/utwente/anonymization/gui/DataviewController.java
@@ -6,10 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import nl.willemsenmedia.utwente.anonymization.data.DataAttribute;
@@ -17,6 +17,9 @@ import nl.willemsenmedia.utwente.anonymization.data.DataEntry;
 import nl.willemsenmedia.utwente.anonymization.data.handling.AnonimizationController;
 import nl.willemsenmedia.utwente.anonymization.settings.Settings;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -34,11 +37,17 @@ public class DataviewController implements Initializable {
 	public GridPane raw_data_pane;
 	public ScrollPane anonymous_data_scrollpane;
 	public ScrollPane raw_data_scrollpane;
-	public TableView<ObservableList> table_anonymous_pane;
-	public TableView<ObservableList> table_raw_pane;
+	public TableView<ObservableList<String>> table_anonymous_pane;
+	public TableView<ObservableList<String>> table_raw_pane;
 	private Settings settings;
-	private ObservableList<ObservableList> table_anonymous_data;
-	private ObservableList<ObservableList> table_raw_data;
+	private ObservableList<ObservableList<String>> table_anonymous_data;
+	private ObservableList<ObservableList<String>> table_raw_data;
+
+	private static void copy_to_clipboard(String stringToCopy) {
+		StringSelection stringSelection = new StringSelection(stringToCopy);
+		Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clpbrd.setContents(stringSelection, null);
+	}
 
 	public void bind(DoubleProperty doubleProperty) {
 		progressBar.progressProperty().bind(doubleProperty);
@@ -65,41 +74,36 @@ public class DataviewController implements Initializable {
 	}
 
 	public void addData(DataEntry raw_entry, DataEntry anonymous_entry) {
-		//Add anonymous data
-//		HBox hbox = new HBox();
-//		hbox.getChildren().add(new Label(StringUtils.join(anonimous_entry.getDataAttributes().stream().map(DataAttribute::getData).collect(Collectors.toList()), AttributeSeparator).trim()));
-//		hbox.setAlignment(Pos.TOP_LEFT);
-//		anonymous_data_pane.addRow(anonymous_data_pane.getChildren().size(), hbox);
-//		anonymous_data_pane.addRow(anonymous_data_pane.getChildren().size(), new Separator());
-//		//Add raw data
-//		hbox = new HBox();
-//		hbox.getChildren().add(new Label(StringUtils.join(raw_entry.getDataAttributes().stream().map(DataAttribute::getData).collect(Collectors.toList()), AttributeSeparator).trim()));
-//		hbox.setAlignment(Pos.TOP_LEFT);
-//		raw_data_pane.addRow(anonymous_data_pane.getChildren().size(), hbox);
-//		raw_data_pane.addRow(anonymous_data_pane.getChildren().size(), new Separator());
-
 		//This will create the table
 		if (table_raw_data == null || table_raw_data.size() == 0) {
 			table_raw_data = FXCollections.observableArrayList();
-//			table_raw_pane = new TableView<>();
-//			raw_data_pane.addRow(0, table_raw_pane);
 			table_raw_pane.setItems(table_raw_data);
 			for (DataAttribute dataAttribute : raw_entry.getHeaders()) {
-				TableColumn<ObservableList, String> col = new TableColumn<>(dataAttribute.getData());
+				TableColumn<ObservableList<String>, String> col = new TableColumn<>(dataAttribute.getData());
 				col.setCellValueFactory(param -> new ReadOnlyStringWrapper((String) param.getValue().get(raw_entry.getHeaders().indexOf(dataAttribute))));
 				table_raw_pane.getColumns().add(col);
 			}
+			table_raw_pane.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+				if (event.isControlDown() && KeyCode.C.equals(event.getCode())) {
+					TablePosition cell = table_raw_pane.getFocusModel().getFocusedCell();
+					copy_to_clipboard(table_raw_data.get(cell.getRow()).get(cell.getColumn()));
+				}
+			});
 		}
 		if (table_anonymous_data == null || table_anonymous_data.size() == 0) {
 			table_anonymous_data = FXCollections.observableArrayList();
-//			table_anonymous_pane = new TableView<>();
-//			anonymous_data_pane.addRow(0, table_anonymous_pane);
 			table_anonymous_pane.setItems(table_anonymous_data);
 			for (DataAttribute dataAttribute : anonymous_entry.getHeaders()) {
-				TableColumn<ObservableList, String> col = new TableColumn<>(dataAttribute.getData());
+				TableColumn<ObservableList<String>, String> col = new TableColumn<>(dataAttribute.getData());
 				col.setCellValueFactory(param -> new ReadOnlyStringWrapper((String) param.getValue().get(anonymous_entry.getHeaders().indexOf(dataAttribute))));
 				table_anonymous_pane.getColumns().add(col);
 			}
+			table_anonymous_pane.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+				if (event.isControlDown() && KeyCode.C.equals(event.getCode())) {
+					TablePosition cell = table_anonymous_pane.getFocusModel().getFocusedCell();
+					copy_to_clipboard(table_anonymous_data.get(cell.getRow()).get(cell.getColumn()));
+				}
+			});
 		}
 		//Now add all the data
 		table_raw_data.add(FXCollections.observableArrayList(raw_entry.getDataAttributes().stream().map(DataAttribute::getData).collect(Collectors.toList())));
