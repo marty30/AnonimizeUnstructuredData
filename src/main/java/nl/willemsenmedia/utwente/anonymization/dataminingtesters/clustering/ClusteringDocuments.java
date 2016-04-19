c06a0a9a5d120ab0d25c03960a68f93d5e2a88ad
Martijn Willemsen <m.j.willemsen@student.utwente.nl>
2016-04-19 16:17:16 +0200

    Clustering wordt nu ook vergeleken.

--- src/main/java/nl/willemsenmedia/utwente/anonymization/dataminingtesters/clustering/ClusteringDocuments.java
+++ src/main/java/nl/willemsenmedia/utwente/anonymization/dataminingtesters/clustering/ClusteringDocuments.java
@@ -16,13 +16,11 @@ import nl.willemsenmedia.utwente.anonymization.data.DataEntry;
 import nl.willemsenmedia.utwente.anonymization.data.reading.FileReader;
 import nl.willemsenmedia.utwente.anonymization.settings.Settings;
 import org.carrot2.clustering.lingo.LingoClusteringAlgorithm;
-import org.carrot2.clustering.synthetic.ByUrlClusteringAlgorithm;
 import org.carrot2.core.*;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * This example shows how to cluster a set of documents available as an {@link ArrayList}.
@@ -31,9 +29,9 @@ import java.util.List;
  * it's better to implement a {@link IDocumentSource} for the custom document source, so
  * that e.g., the {@link Controller} can cache its results, if needed.
  */
+@SuppressWarnings("Duplicates")
 public class ClusteringDocuments {
-	public static void main(String[] args) throws JAXBException {
-		/* [[[start:clustering-document-list-intro]]]
+	/* [[[start:clustering-document-list-intro]]]
 		 *
          * <div>
          * <p>
@@ -57,44 +55,115 @@ public class ClusteringDocuments {
          *
          * [[[end:clustering-document-list-intro]]]
          */
-		{
-			// [[[start:clustering-document-list]]]
-            /* A few example documents, normally you would need at least 20 for reasonable clusters. */
-			final String[][] data = new String[][]
-					{
-							{
-									"http://en.wikipedia.org/wiki/Data_mining",
-									"Data mining - Wikipedia, the free encyclopedia",
-									"Article about knowledge-discovery in databases (KDD), the practice of automatically searching large stores of data for patterns."
-							},
+	public static void main(String[] args) throws JAXBException {
+		/* Prepare Carrot2 documents */
+		final ArrayList<Document> documents_raw = new ArrayList<>();
+		final ArrayList<Document> documents_anon = new ArrayList<>();
 
-							{
-									"http://www.ccsu.edu/datamining/resources.html",
-									"CCSU - Data Mining",
-									"A collection of Data Mining links edited by the Central Connecticut State University ... Graduate Certificate Program. Data Mining Resources. Resources. Groups ..."
-							},
+		List<DataEntry> dataEntries_raw = FileReader.readFile(new File("C:\\Users\\Martijn\\Dropbox\\Studie\\College\\Module11&12\\ResearchProject-Ynformed\\JavaApplicatie\\AnonimizeUnstructuredData\\SFU_Review_Corpus.csv"), Settings.getDefault(), null);
+		List<DataEntry> dataEntries_anon = FileReader.readFile(new File("C:\\Users\\Martijn\\Dropbox\\Studie\\College\\Module11&12\\ResearchProject-Ynformed\\JavaApplicatie\\AnonimizeUnstructuredData\\SFU_Review_Corpus_anonimous.csv"), Settings.getDefault(), null);
+		for (DataEntry dataEntry : dataEntries_raw) {
+			StringBuilder content = new StringBuilder();
+			for (DataAttribute atrr : dataEntry.getDataAttributes()) {
+				content.append(atrr.getData());
+			}
+			documents_raw.add(new Document(dataEntry.getHeaders().get(0).getData(), content.toString()));
+		}
+		for (DataEntry dataEntry : dataEntries_anon) {
+			StringBuilder content = new StringBuilder();
+			for (DataAttribute atrr : dataEntry.getDataAttributes()) {
+				content.append(atrr.getData());
+			}
+			documents_anon.add(new Document(dataEntry.getHeaders().get(0).getData(), content.toString()));
+		}
 
-							{
-									"http://www.kdnuggets.com/",
-									"KDnuggets: Data Mining, Web Mining, and Knowledge Discovery",
-									"Newsletter on the data mining and knowledge industries, offering information on data mining, knowledge discovery, text mining, and web mining software, courses, jobs, publications, and meetings."
-							},
+		/* A controller to manage the processing pipeline. */
+		final Controller controller = ControllerFactory.createSimple();
 
-							{
-									"http://en.wikipedia.org/wiki/Data-mining",
-									"Data mining - Wikipedia, the free encyclopedia",
-									"Data mining is considered a subfield within the Computer Science field of knowledge discovery. ... claim to perform \"data mining\" by automating the creation ..."
-							},
+		/* Perform clustering by topic using the Lingo algorithm.*/
+		final ProcessingResult RawByTopicClusters = controller.process(documents_raw, null, LingoClusteringAlgorithm.class);
+		final List<Cluster> RawClustersByTopic = RawByTopicClusters.getClusters();
+		final ProcessingResult AnonByTopicClusters = controller.process(documents_anon, null, LingoClusteringAlgorithm.class);
+		final List<Cluster> AnonClustersByTopic = AnonByTopicClusters.getClusters();
 
-							{
-									"http://www.anderson.ucla.edu/faculty/jason.frand/teacher/technologies/palace/datamining.htm",
-									"Data Mining: What is Data Mining?",
-									"Outlines what knowledge discovery, the process of analyzing data from different perspectives and summarizing it into useful information, can do and how it works."
-							},
-					};
+		//Now compare the two sets of clusters
+		List<Double> percentages_best_possible = new LinkedList<>();
+		List<Double> percentages_one_to_one = new LinkedList<>();
+		Map<Cluster, Map<Cluster, Integer>> possible_matches = new HashMap<>(RawClustersByTopic.size());
+		for (Cluster raw_cluster : RawClustersByTopic) {
+			//We have a raw cluster, now determine the best anonymous cluster
+			Map<Cluster, Integer> matches_for_raw_cluster = new HashMap<>(AnonClustersByTopic.size());
+			for (Cluster anon_cluster : AnonClustersByTopic) {//.get(RawClustersByTopic.indexOf(raw_cluster));
+				int the_same = 0;
+				for (Document raw_doc : raw_cluster.getAllDocuments()) {
+					if (anon_cluster.getAllDocuments().stream().anyMatch(anon_doc -> anon_doc.getStringId().equals(raw_doc.getStringId()))) {
+						the_same++;
+					}
+				}
+				if (Objects.equals(raw_cluster.getId(), anon_cluster.getId()))
+					percentages_one_to_one.add((double) the_same / raw_cluster.size());
+				matches_for_raw_cluster.put(anon_cluster, the_same);
+			}
+			possible_matches.put(raw_cluster, sortByValue(matches_for_raw_cluster));
+			Map.Entry<Cluster, Integer> best_match = matches_for_raw_cluster.entrySet().stream().max((o1, o2) -> o1.getValue().compareTo(o2.getValue())).orElse(new DefaultEntry<>(null, 0));
+			percentages_best_possible.add((double) best_match.getValue() / raw_cluster.size());
+		}
 
-            /* Prepare Carrot2 documents */
-			final ArrayList<Document> documents = new ArrayList<Document>();
+		// Find the optimal match between the two lists
+		// TODO: 19-4-2016 Checken of dit inderdaad optimaal is
+		Map<Cluster, Map.Entry<Cluster, Integer>> real_matches = new LinkedHashMap<>(RawClustersByTopic.size());
+		//Now find the best match. To do this in an optimal way, we first need to find the two clusters with the most similarities.
+		for (Map.Entry<Cluster, Map<Cluster, Integer>> entry : possible_matches.entrySet()) {
+			Cluster raw_cluster = entry.getKey();
+			for (Map.Entry<Cluster, Integer> current_match : entry.getValue().entrySet()) {
+				if (real_matches.entrySet().stream().noneMatch(clusterMapEntry -> clusterMapEntry.getValue().getKey().equals(current_match.getKey()))) {
+					real_matches.put(raw_cluster, current_match);
+					break;
+				}
+			}
+		}
+
+//		ConsoleFormatter.displayClusters(RawClustersByTopic);
+//		ConsoleFormatter.displayClusters(AnonClustersByTopic);
+		System.out.println("Equality (one-to-one): " + percentages_one_to_one.stream().mapToDouble(a -> a).average().orElse(0));
+		System.out.println("Equality (best possible): " + percentages_best_possible.stream().mapToDouble(a -> a).average().orElse(0));
+		System.out.println("Equality (optimal match): " + real_matches.entrySet().stream().mapToDouble((entry) -> (double) entry.getValue().getValue() / entry.getKey().size()).average().orElse(0));
+		//print de matches
+		for (Map.Entry<Cluster, Map.Entry<Cluster, Integer>> match : real_matches.entrySet()) {
+			System.out.println(match.getKey().getId() + " -> " + match.getValue().getKey().getId() + " (" + match.getValue().getValue() + ")");
+		}
+	}
+
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		Map<K, V> result = new LinkedHashMap<>();
+		map.entrySet().stream().sorted((c1, c2) -> c2.getValue().compareTo(c1.getValue())).forEachOrdered(e -> result.put(e.getKey(), e.getValue()));
+		return result;
+	}
+
+	private static class DefaultEntry<K, V> implements Map.Entry<K, V> {
+		private K key;
+		private V value;
+
+		public DefaultEntry(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		@Override
+		public K getKey() {
+			return key;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public V setValue(V value) {
+			return this.value = value;
+		}
+	}
 //			File folder = new File("path to dir");
 //			String temp;
 //			try {
@@ -124,42 +193,4 @@ public class ClusteringDocuments {
 //				documents.add(new Document(row[1], row[2], row[0]));
 //			}
 
-
-
-//			List<DataEntry> dataEntries = FileReader.readFile(new File("C:\\Users\\Martijn\\Dropbox\\Studie\\College\\Module11&12\\ResearchProject-Ynformed\\JavaApplicatie\\AnonimizeUnstructuredData\\SFU_Review_Corpus.csv"), Settings.getDefault(), null);
-			List<DataEntry> dataEntries = FileReader.readFile(new File("C:\\Users\\Martijn\\Dropbox\\Studie\\College\\Module11&12\\ResearchProject-Ynformed\\JavaApplicatie\\AnonimizeUnstructuredData\\export_2016-04-04_163842.684.csv"), Settings.getDefault(), null);
-			for (DataEntry dataEntry: dataEntries){
-				StringBuilder content = new StringBuilder();
-				for (DataAttribute atrr : dataEntry.getDataAttributes()) {
-					content.append(atrr.getData());
-				}
-				documents.add(new Document(dataEntry.getHeaders().get(0).getData(), content.toString()));
-			}
-
-            /* A controller to manage the processing pipeline. */
-			final Controller controller = ControllerFactory.createSimple();
-
-            /*
-             * Perform clustering by topic using the Lingo algorithm. Lingo can
-             * take advantage of the original query, so we provide it along with the documents.
-             */
-			final ProcessingResult byTopicClusters = controller.process(documents, "data mining",
-					LingoClusteringAlgorithm.class);
-			final List<Cluster> clustersByTopic = byTopicClusters.getClusters();
-
-            /* Perform clustering by domain. In this case query is not useful, hence it is null. */
-			final ProcessingResult byDomainClusters = controller.process(documents, null,
-					ByUrlClusteringAlgorithm.class);
-			final List<Cluster> clustersByDomain = byDomainClusters.getClusters();
-			// [[[end:clustering-document-list]]]
-
-			ConsoleFormatter.displayClusters(clustersByTopic);
-			System.out.println("---------------------------------");
-			ConsoleFormatter.displayClusters(clustersByDomain);
-			System.out.println("---------------------------------");
-			ConsoleFormatter.displayResults(controller.process(documents, "data mining",
-					LingoClusteringAlgorithm.class, ByUrlClusteringAlgorithm.class));
-
-		}
-	}
 }
